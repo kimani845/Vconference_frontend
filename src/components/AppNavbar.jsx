ae635cc895f56b6e5b4c8c639eb271c367cf207d
mike kimani <kimanimichael09@gmail.com>
2025-05-08 01:21:28 +0300

    updated forgot password

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,35 +1,64 @@
-import {Avatar, Dropdown, DropdownButton, Nav, Navbar } from 'flowbite-react';
-// import UserIcon from "../assets/user.png";
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-
 const AppNavbar = (props) => {
-    let navigate = useNavigate();
+  const navigate = useNavigate();
+  const { IsLoggedIn, setIsLoggedIn, name, setName, email, setEmail } = props;
 
-    const{IsLoggedIn, setIsLoggedIn, name , setName, email, setEmail} = props;
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setName('');
+    setEmail('');
+    toast.success('Logged Out Successfully');
+    navigate('/');
+  };
 
-    const handleLogout = () => {
-        setIsLoggedIn(false);
-        setName("");
-        setEmail("");
-        toast.success("Logged Out Successfully");
-        navigate("/");
-    }
-    return () => {
-        <Navbar className="bg-white" fluid={true} rounded={true}>
-            <Navbar.Brand>  
-                <Link to="/">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                        Video Conference
-                    </span>
-                </Link>
-            </Navbar.Brand>
-    
-            </Navbar>
-    }
-}   
+  return (
+    <Navbar className="bg-white shadow" fluid={true} rounded={true}>
+      <Navbar.Brand>
+        <Link to="/">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Video Conference
+          </span>
+        </Link>
+      </Navbar.Brand>
+
+      <div className="flex md:order-2">
+        {IsLoggedIn ? (
+          <Dropdown
+            inline
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">{name}</span>
+              <span className="block truncate text-sm font-medium">{email}</span>
+            </Dropdown.Header>
+            <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link
+            to="/login"
+            className="text-sm text-blue-600 hover:underline font-semibold"
+          >
+            Login
+          </Link>
+        )}
+      </div>
+    </Navbar>
+  );
+};
 
 export default AppNavbar;
--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,1 +1,15 @@
 
+// ForgotPassword
+
+import React from 'react';
+
+const ForgotPassword = () =>{
+    return (
+        <div> 
+            <h2>
+                Forgot Password
+            </h2>
+      {/* Forgot password form content */}
+        </div>
+    )
+};
