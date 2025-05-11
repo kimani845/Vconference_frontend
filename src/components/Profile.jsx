aedfc7a33f993ddf77f9d350fed323469dd6c844
mike kimani <kimanimichael09@gmail.com>
2025-05-12 01:23:07 +0300

    added a profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,18 +4,24 @@ import { Link } from 'react-router-dom';
 import {useAuth} from './AuthContext';
 
 const Profile = ({ name, setName, email, setEmail, password, token, setToken }) => {
-    return (
-        <div>
-            <h1>Profile Page</h1>
-            <p>Name: {name}</p>
-            <p>setName: {setName}</p>
-            <p>Email: {email}</p>
-            <p>SetEmail: {setEmail}</p>
-            <p>Password0: {password}</p>
-            <p>Token: {token}</p>
-            <p>SetToken: {setToken}</p>
+    const user = useAuth();
+    // const [showPassword, setShowPassword] = React.useState(false);
+    // const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
 
-        </div>
+    return (
+        <>
+            <div>
+                <h1>Profile Page</h1>
+                <p>Name: {name}</p>
+                <p>setName: {setName}</p>
+                <p>Email: {email}</p>
+                <p>SetEmail: {setEmail}</p>
+                <p>Password0: {password}</p>
+                <p>Token: {token}</p>
+                <p>SetToken: {setToken}</p>
+
+            </div>
+        </>
     );
     };
 
