6788fce470d42e97a60e5472d951e4ec5e163513
mike kimani <kimanimichael09@gmail.com>
2025-05-12 03:09:09 +0300

    updated the register page

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const App = () => {
                 />
                 
 
-            <Route path = "/signup" element = {
+            <Route path = "/Register" element = {
                     <Register
                         isLoggedIn = {isLoggedIn} 
                         setIsLoggedIn = {setIsLoggedIn} 
