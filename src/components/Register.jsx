a882382332d3808f8c2f42b479a5c293b2ad42bf
mike kimani <kimanimichael09@gmail.com>
2025-05-12 04:43:40 +0300

    updated the register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -61,10 +61,11 @@ const Register = (props) => {
 
 
         return (
+            
     <div className="flex h-screen items-center justify-center">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center text-[18px]">
             Create an account
             </h1>
             <form
