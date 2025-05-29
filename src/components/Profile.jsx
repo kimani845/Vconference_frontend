551c77306a34e1d8748a350de0107e566fc34b8a
mike kimani <kimanimichael09@gmail.com>
2025-05-30 00:13:49 +0300

    message

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -17,14 +17,51 @@ const Profile = (props) =>{
     const { isLoggedIn , name, email , picture, idToken } = props;
     useEffect(() =>{
         const fetchUser = async () => {
-            if (isLoggedIn == false ) redirect("/");
-            {
-                return;
-                
-                }
+            if (isLoggedIn === false ) redirect("/");
+            // try {
+            //     const response = await fetch('/api/user') {
+            //         method: 'GET',
+            //         headers: {
+            //             'Content-Type': 'application/json',
+                        
+            //         },
+            //     }
+            // }
+            
             }
-        }
-    )
+        }, []      
+    );
+    return (
+        <div className = "flex items-center justify-center mt-5">
+            <div className = "w-full max-w-lg p-6 bg-white boarder border-grey-200 rounded-lg shadow dark:bg-grey-800 dark:border-gray-700">
+                <div className = " flex flex-col items-center pb-10">
+                    <img alt = "User Icon" width="96" height="96" src = {UserIcon} className = " mb-3 rounded-full shadow-lg"/>
+                    <h5 className = " mb-1 text-x1 font-medium text-gray-900 dark:text-white">{name}</h5>
+                    <span className = " text-sm text-gray-500 dark:text-gray-400">{email}</span>
+                    <div className = "mt-4 flex space-x-3 lg:mt-6">
+                        <a href =  "#" 
+                        className="inline-flex items-center rounded-lg
+                                bg-purple-700 px-4 py-2 text-center text-sm font-medium text-white
+                                    hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:bg-purple-600
+                                        dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                                        Add friend
+                        </a>
+
+                        <a href="#"
+                        className="inline-flex items-center rounded-lg border border-gray-300
+                                bg-white px-4 py-2 text-center text-sm font-medium text-gray-900
+                                    hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200
+                                        dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700
+                                            dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                                                Message
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 // const Profile = ({ name, setName, email, setEmail, password, token, setToken }) => {
