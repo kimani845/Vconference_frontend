301d1ad786d0dda36c1fe220134e2fffce9a7426
mike kimani <kimanimichael09@gmail.com>
2025-05-29 23:39:45 +0300

    message

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,42 +1,65 @@
 // Profile
+import { useEffect , useState } from 'react';
+import { UserIcon } from '@heroicons/react/outline';
+import { redirect } from 'react-router-dom';
 import React from 'react';
 import { Link } from 'react-router-dom';
 // import {useAuth} from './AuthContext';
 // import { useAuth0 } from '@auth0/auth0-react';
 
-const Profile = ({ name, setName, email, setEmail, password, token, setToken }) => {
-    const user = useAuth();
-    // const [showPassword, setShowPassword] = React.useState(false);
-    // const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+const Profile = (props) =>{
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [userProfile, setUserProfile] = useState({});
+    const [userProfileData, setUserProfileData] = useState({});
+    const [userProfilePicture, setUserProfilePicture] = useState({});
+    const { isLoggedIn , name, email , picture, idToken } = props;
+    useEffect(() =>{
+        const fetchUser = async () => {
+            if (isLoggedIn == false ) redirect("/");
+            {
+                return;
+                
+                }
+            }
+        }
+    )
+}
 
-    return (
-        // pages/Home.js
-    <div>
-        <h2 className="font-bold my-5 text-xl text-center">
-            This is simple home page. 
-        </h2>
-            <div className ="profile">
-                <p className ="userInfo" id ="userInfo1">
-                    Name : {user.name}</p>
-                <p className ="userInfo" id = "userInfo2">
-                    Name: {name2}</p>
-                <p className= "userInfo" id = "userInfo3">
-                    setName: {setName}</p>
-                <p className = "userInfo" id = "userEmail">
-                    Email: {email}</p>
-                <p className = "userName" id = "userEmail2">
-                    SetEmail: {setEmail}</p>
-                <p className = "userInfo" id ="userPassword">
-                    Password0: {password}</p>
-                <p className ="userInfo" id = "userToken">
-                    Token: {token}</p>
-                <p className="userInfo" id ="userToken">
-                    SetToken: {setToken}</p>
+// const Profile = ({ name, setName, email, setEmail, password, token, setToken }) => {
+//     const user = useAuth();
+//     // const [showPassword, setShowPassword] = React.useState(false);
+//     // const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
 
-            </div>
-        </div>
+//     return (
+//         // pages/Home.js
+//     <div>
+//         <h2 className="font-bold my-5 text-xl text-center">
+//             This is simple home page. 
+//         </h2>
+//             <div className ="profile">
+//                 <p className ="userInfo" id ="userInfo1">
+//                     Name : {user.name}</p>
+//                 <p className ="userInfo" id = "userInfo2">
+//                     Name: {name2}</p>
+//                 <p className= "userInfo" id = "userInfo3">
+//                     setName: {setName}</p>
+//                 <p className = "userInfo" id = "userEmail">
+//                     Email: {email}</p>
+//                 <p className = "userName" id = "userEmail2">
+//                     SetEmail: {setEmail}</p>
+//                 <p className = "userInfo" id ="userPassword">
+//                     Password0: {password}</p>
+//                 <p className ="userInfo" id = "userToken">
+//                     Token: {token}</p>
+//                 <p className="userInfo" id ="userToken">
+//                     SetToken: {setToken}</p>
+
+//             </div>
+//         </div>
         
-    );
-    };
+//     );
+//     };
 
 export default Profile; 
