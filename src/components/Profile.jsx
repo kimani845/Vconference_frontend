823c07e9c4b14db408de6d5ccf9a7a584c73dd5d
mike kimani <kimanimichael09@gmail.com>
2025-05-29 20:53:59 +0300

    message

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,28 +10,15 @@ const Profile = ({ name, setName, email, setEmail, password, token, setToken }) 
     // const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
 
     return (
-        <>
-            <div className ="profile">
-                <p className ="userInfo" id ="userInfo1">
-                    Name : {user.name}</p>
-                <p className ="userInfo" id = "userInfo2">
-                    Name: {name2}</p>
-                <p className= "userInfo" id = "userInfo3">
-                    setName: {setName}</p>
-                <p className = "userInfo" id = "userEmail">
-                    Email: {email}</p>
-                <p className = "userName" id = "userEmail2">
-                    SetEmail: {setEmail}</p>
-                <p className = "userInfo" id ="userPassword">
-                    Password0: {password}</p>
-                <p className ="userInfo" id = "userToken">
-                    Token: {token}</p>
-                <p className="userInfo" id ="userToken">
-                    SetToken: {setToken}</p>
+     // pages/Home.js
 
-            </div>
-        </>
+    <div>
+        <h2 className="font-bold my-5 text-xl text-center">This is simple home page. Customize as per your usecase</h2>
+    </div>
     );
-    };
+};
+
+
+
 
 export default Profile; 
